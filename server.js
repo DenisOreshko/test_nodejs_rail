88a916b745d2ea5ea7920b0c885a073c17a616fa
Denis <denisoreshkoofficial@gmail.com>
2023-10-26 11:25:17 +0300

    del brain

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    const net = new brain.recurrent.LSTMTimeStep({
+    /* const net = new brain.recurrent.LSTMTimeStep({
         inputSize: 5,
         hiddenLayers: [15,25,25],
         outputSize: 5
-    });
+    }); */
 	res.send('Server run')
 })
 
